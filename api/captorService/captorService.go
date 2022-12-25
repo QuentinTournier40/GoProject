1e51c5cd04115e1a7a84d7dd3b829b900b26c5a4
QuentinTournier40 <qtournier@iutbayonne.univ-pau.fr>
2022-12-25 13:46:28 +0100

    feat: ajout d'un nouveau endpoint

--- api/captorService/captorService.go
+++ api/captorService/captorService.go
@@ -45,6 +45,12 @@ type DataMeasureCodeIata struct {
 	VALUE string `json:"value"`
 }
 
+type DataCaptorIataCode struct {
+	IATA       string         `json:"iata"`
+	CAPTORNAME string         `json:"captorName"`
+	VALUES     []*DataMeasure `json:"values"`
+}
+
 func GetDataFromAllCaptors(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var dataValues []*Data
@@ -112,7 +118,29 @@ func GetDataByCaptor(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetDataByIataCodeAndCaptor(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Get data from one captorService but only in one airport")
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	iataCode := vars["iataCode"]
+	captorName := vars["captorName"]
+	var captorData []*DataMeasure
+	var keys []string
+	switch strings.ToLower(captorName) {
+	case "pressure":
+		keys = bdd.GetAllKeyRegex(strings.ToUpper(iataCode) + "/PRESSURE/*")
+	case "temperature":
+		keys = bdd.GetAllKeyRegex(strings.ToUpper(iataCode) + "/TEMPERATURE/*")
+	case "wind":
+		keys = bdd.GetAllKeyRegex(strings.ToUpper(iataCode) + "/WIND/*")
+	}
+
+	for _, key := range keys {
+		data := bdd.GetValue(key)
+		splitKey := strings.Split(key, "/")
+		captorData = append(captorData, &DataMeasure{DATE: splitKey[2], VALUE: data})
+	}
+	p, _ := json.Marshal(DataCaptorIataCode{IATA: iataCode, CAPTORNAME: captorName, VALUES: captorData})
+	w.Write(p)
+
 }
 
 func GetDataByIataCodeAndCaptorAndYear(w http.ResponseWriter, r *http.Request) {
